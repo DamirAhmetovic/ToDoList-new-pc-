a0b59db7a1bf582404386ae636808facb5cb7e10
damir <Damir02@hotmail.nl>
2021-03-17 15:03:12 +0100

    list index bug fixed

--- List.php
+++ List.php
@@ -1,6 +1,6 @@
 <?php
 include "datalayer.php";
-if (! isset($_GET["list_id"])) {
+if (!isset($_GET["list_id"])) {
     die("Error: no list_id given.");
 }
 
@@ -12,13 +12,11 @@ switch ($_GET['sort_column']) {
     case 'status':
         $task_order = ' ORDER BY task_status';
         break;
-} 
+}
 
-
-$list = GetList($_GET["list_id"]);
-
-//echo '<pre>' . var_dump($list) . ' </pre>';
-//die();
+$list_id = $_GET["list_id"];
+$list = GetList($list_id);
+$sort = $_GET['sort_column'];
 
 include "include/header.php";
 ?>
@@ -26,55 +24,53 @@ include "include/header.php";
 <form action="TaskAdd.php?list_id=" method="post">
     <button class="w3-button w3-green">Add</button>
 </form>
-<?php 
-    $tasks = GetTasksForLists($list['list_id'], $task_order);
-    ?>
-    <div class="list_name">
-        <h2> <?php echo $list['list_name']; ?>
-            <form action="ListEdit.php?list_id=<?php echo $list['list_id']; ?>" method="post">
-                <button class="w3-button w3-red">edit</button>
-            </form>
-        </h2>
-        <div class="w3-row-padding w3-padding-64 w3-container">
-            <table class="w3-table-all w3-small">
-                <thead>
-                    <tr class="w3-red">
-                        <th>id</th>
-                        <th>task</th>
-                        <th><a href="http://localhost/jaar%202/ToDoList/List.php?list_id=31&sort_column=duration">duration</a></th>
-                        <th>status</th>
-                        <th>edit</th>
-                        <th>delete</th>
-                    </tr>
-                    <?php foreach ($tasks as $task) {
-                    ?>
-
-                        <tr>
-                            <td><?php echo $task['task_list_id']; ?></td>
-                            <td><?php echo $task['task_name']; ?></td>
-                            <td><?php echo $task['task_duration']; ?></td>
-                            <td><?php echo $task['task_status']; ?></td>
-                            <td>
-                                <form action="TaskEdit.php?task_id=<?php echo $task['task_id']; ?>" method="post">
-                                    <button class="w3-button w3-red">+</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="TaskDelete.php?task_id=<?php echo $task['task_id']; ?>" method="post">
-                                    <button class="w3-button w3-red">-</button>
-                                </form>
-                            </td>
-                        </tr>
-                    <?php
-                    } ?>
-            </table>
-        </div>
-        <form action="ListDelete.php?list_id=<?php echo $list['list_id']; ?>" method="post">
-            <button class="w3-button w3-red">delete</button>
+<?php
+$tasks = GetTasksForLists($list['list_id'], $task_order);
+?>
+<div class="list_name">
+    <h2> <?php echo $list['list_name']; ?>
+        <form action="ListEdit.php?list_id=<?php echo $list['list_id']; ?>" method="post">
+            <button class="w3-button w3-red">edit</button>
         </form>
-    </div>
-s
+    </h2>
+    <div class="w3-row-padding w3-padding-64 w3-container">
+        <table class="w3-table-all w3-small">
+            <thead>
+                <tr class="w3-red">
+                    <th>id</th>
+                    <th>task</th>
+                    <th><a href="http://localhost/jaar%202/ToDoList/List.php?list_id=<?= $list_id ?>&sort_column=duration">duration</a></th>
+                    <th>status</th>
+                    <th>edit</th>
+                    <th>delete</th>
+                </tr>
+                <?php foreach ($tasks as $task) {
+                ?>
 
+                    <tr>
+                        <td><?php echo $task['task_list_id']; ?></td>
+                        <td><?php echo $task['task_name']; ?></td>
+                        <td><?php echo $task['task_duration']; ?></td>
+                        <td><?php echo $task['task_status']; ?></td>
+                        <td>
+                            <form action="TaskEdit.php?task_id=<?php echo $task['task_id']; ?>" method="post">
+                                <button class="w3-button w3-red">+</button>
+                            </form>
+                        </td>
+                        <td>
+                            <form action="TaskDelete.php?task_id=<?php echo $task['task_id']; ?>" method="post">
+                                <button class="w3-button w3-red">-</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php
+                } ?>
+        </table>
+    </div>
+    <form action="ListDelete.php?list_id=<?php echo $list['list_id']; ?>" method="post">
+        <button class="w3-button w3-red">delete</button>
+    </form>
+</div>
 <?php
 include "include/footer.php";
 ?>
--- ListIndex.php
+++ ListIndex.php
@@ -6,53 +6,20 @@ include "include/header.php";
 ?>
 <!-- First Grid -->
 <form action="TaskAdd.php?list_id=" method="post">
-    <button class="w3-button w3-green">Add</button>
+    <button class="w3-button w3-green">Add a Task</button>
 </form>
-<?php foreach ($lists as $list) { 
-    $tasks = GetTasksForLists($list['list_id']);
-    ?>
+<?php foreach ($lists as $list) {
+?>
     <div class="list_name">
-        <h2> <?php echo $list['list_name']; ?>
-            <form action="ListEdit.php?list_id=<?php echo $list['list_id']; ?>" method="post">
-                <button class="w3-button w3-red">edit</button>
+        <h2> <?php echo $list['list_name']; ?> </h2>
+        <form action="ListEdit.php?list_id=<?php echo $list['list_id']; ?>" method="post">
+                <button class="btn btn-outline-danger">Edit</button>
             </form>
-        </h2>
-        <div class="w3-row-padding w3-padding-64 w3-container">
-            <table class="w3-table-all w3-small">
-                <thead>
-                    <tr class="w3-red">
-                        <th>id</th>
-                        <th>task</th>
-                        <th>duration</th>
-                        <th>status</th>
-                        <th>edit</th>
-                        <th>delete</th>
-                    </tr>
-                    <?php foreach ($tasks as $task) {
-                    ?>
-
-                        <tr>
-                            <td><?php echo $task['task_list_id']; ?></td>
-                            <td><?php echo $task['task_name']; ?></td>
-                            <td><?php echo $task['task_duration']; ?></td>
-                            <td><?php echo $task['task_status']; ?></td>
-                            <td>
-                                <form action="TaskEdit.php?task_id=<?php echo $task['task_id']; ?>" method="post">
-                                    <button class="w3-button w3-red">+</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="TaskDelete.php?task_id=<?php echo $task['task_id']; ?>" method="post">
-                                    <button class="w3-button w3-red">-</button>
-                                </form>
-                            </td>
-                        </tr>
-                    <?php
-                    } ?>
-            </table>
-        </div>
         <form action="ListDelete.php?list_id=<?php echo $list['list_id']; ?>" method="post">
-            <button class="w3-button w3-red">delete</button>
+            <button class="btn btn-outline-danger">Delete</button>
+        </form>
+        <form action="List.php?list_id=<?php echo $list['list_id']; ?>" method="post">
+            <button class="btn btn-outline-danger">List</button>
         </form>
     </div>
 <?php } ?>
